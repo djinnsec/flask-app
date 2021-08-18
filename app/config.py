1f9284841d09ae04425cc274c39066d9b9b0b7e8
djinnsec <50659164+djinnsec@users.noreply.github.com>
2021-08-18 19:52:17 +0200

    Delete app directory
    restart from hello world

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-WTF_CSRF_ENABLED = True
-
-# set this environment variable and modify the name accordingly when deploying
-SECRET_KEY = os.getenv('BASIC_FLASK_SECRET', 'you-will-never-guess')
